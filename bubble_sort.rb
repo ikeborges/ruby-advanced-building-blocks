736dac7d86354186ddebd29b7ab2693029acd52c
Kaique Borges <rique.kaique@gmail.com>
2016-11-17 22:47:26 -0200

    Missing the results array

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,8 +2,10 @@ def bubble_sort(arr)
   n = arr.length - 1
 
   n.times do
-    arr.each_cons(2) do |a, b|
-      check = a <=> b
+    arr.map_with_index do |value, index|
+      a = arr[index]
+      b = arr[index + 1]
+      check = index <=> (index + 1)
 
       if check == 1
         a, b = b, a
