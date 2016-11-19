7392a1d77614ee04820a4e07c877483cd7db5f44
Kaique Borges <rique.kaique@gmail.com>
2016-11-19 14:48:45 -0200

    Solved basic bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,22 @@
 def bubble_sort(arr)
   n = arr.length - 1
 
-  n.times do
-    arr.map_with_index do |value, index|
-      a = arr[index]
-      b = arr[index + 1]
-      check = index <=> (index + 1)
+  loop do
+    swapped = false
 
-      if check == 1
-        a, b = b, a
+    n.times do |i| # Checks every item of the array one time in each turn
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true # Only gets evaluated by the "loop" loop when the entire array is checked and there aren't ANY changes
       end
-
     end
+
+    break if not swapped
   end
 
   arr
 end
 
+
 p bubble_sort([4,3,78,2,0,2])
+# [3, 4, 2, 0, 2, 78]
