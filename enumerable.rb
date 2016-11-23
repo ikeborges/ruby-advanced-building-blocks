db9fdd887aa507e8c208ac99ab5a55d1de4d18f6
Kaique Borges <rique.kaique@gmail.com>
2016-11-22 22:16:51 -0200

    Implemented my_none?

--- enumerable.rb
+++ enumerable.rb
@@ -4,7 +4,7 @@ module Enumerable
     length.times do |n|
       yield(self[n])
     end
-    return self
+    self
   end
 
   def my_each_with_index
@@ -16,37 +16,44 @@ module Enumerable
 
   def my_select
     result = []
-    self.my_each do |i|
+    my_each do |i|
       result << i if yield(i)
     end
-    return result
+    result
   end
 
   def my_all?
     if block_given?
       result = []
-      self.my_each do |i|
+      my_each do |i|
         result << i if yield(i)
       end
-      return result == self ? true : false
+      result == self ? true : false
     else # If no block is given
-      self.my_each { |i| return false if i == false  || i == nil }
-      return true
+      my_each { |i| return false if i == false || i.nil? }
+      true
     end
   end
 
   def my_any?
     if block_given?
-      self.my_each { |i| return true if yield(i) }
+      my_each { |i| return true if yield(i) }
     else
-      self.my_each { |i| return true if i }
+      my_each { |i| return true if i }
     end
-    return false
+    false
   end
 
+  def my_none?
+    if block_given?
+      my_each { |i| return false if yield(i) }
+    else
+      my_each { |i| return false if i }
+    end
+    true
+  end
 end
 
-my_array = ["casa", "carro", "pedra", "galinha", "ovo"]
+my_array = %w(casa carro pedra galinha ovo)
 
-
-p my_array.my_any?
+# p my_array.my_none? { |i| i.length == 3 }
